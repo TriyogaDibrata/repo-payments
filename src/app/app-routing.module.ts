91ef7b6500b258f4a3690047cd764a93c117849f
triyogadibrata <triyoga.dibrata@gmail.com>
2022-04-23 13:13:58 +0800

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './_auth/login/login.component';
+import { AppMainComponent } from './_layouts/app.main.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const config : ExtraOptions = {
     useHash : false
@@ -9,6 +11,21 @@ export const routes : Routes = [
     {
         path: 'auth/login',
         component: LoginComponent
+    },
+    {
+        path: 'app',
+        component: AppMainComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
     },
     {
         path: '',
